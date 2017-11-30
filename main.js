b31c9b14fbb7e2b2cd2631ae3d55c59914fe3864
jessie <zhuyudian.jessie@shandagames.com>
2017-11-30 17:23:07 +0800

    ininitial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+'use strict';
+var electron = require('electron');
+var app = electron.app
+var BrowserWindow = electron.BrowserWindow
+
+var mainWindow = null;
+
+var globalShortcut = electron.globalShortcut
+
+var configuration = require('./configuration');
+
+app.on('ready', function() {
+    if (!configuration.readSettings('shortcutKeys')) {
+        configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+    }
+
+    mainWindow = new BrowserWindow({
+        frame: false,
+        height: 700,
+        resizable: false,
+        width: 368
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    setGlobalShortcuts();
+});
+
+function setGlobalShortcuts() {
+    globalShortcut.unregisterAll();
+
+    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+
+    globalShortcut.register(shortcutPrefix + '1', function () {
+        mainWindow.webContents.send('global-shortcut', 0);
+    });
+    globalShortcut.register(shortcutPrefix + '2', function () {
+        mainWindow.webContents.send('global-shortcut', 1);
+    });
+}
+
+var ipc = electron.ipcMain;
+
+ipc.on('close-main-window', function () {
+    app.quit();
+});
+
+var settingsWindow = null;
+
+ipc.on('open-settings-window', function () {
+    if (settingsWindow) {
+        return;
+    }
+
+    settingsWindow = new BrowserWindow({
+        frame: false,
+        height: 200,
+        resizable: false,
+        width: 200
+    });
+
+    settingsWindow.loadURL('file://' + __dirname + '/app/settings.html');
+
+    settingsWindow.on('closed', function () {
+        settingsWindow = null;
+    });
+});
+
+ipc.on('close-settings-window', function () {
+    if (settingsWindow) {
+        settingsWindow.close();
+    }
+});
+
+ipc.on('set-global-shortcuts', function () {
+    setGlobalShortcuts();
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# sound-machine-electron
+## To Use
+```bash
+# Clone this repository
+git clone https://github.com/jessiezhudev/sound-machine-electron.git
+# Go into the repository
+cd sound-machine-electron
+# Install dependencies
+npm install
+# Run the app
+npm start
+# pakaging 
+npm run-script package
+```
